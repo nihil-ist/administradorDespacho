5a516d1e7d831d82a2d974bead39ce6a814bf691
Meow <al244179@edu.uaa.mx>
2025-11-03 00:06:22 -0600

    configura
    aunq necesias loguearte

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,6 +8,8 @@ import { HomeHerramientasComponent } from './herramientas/home-herramientas/home
 import { UsuariosComponent } from './herramientas/usuarios/usuarios.component';
 import { AddUserComponent } from './herramientas/add-user/add-user.component';
 import { DirectorioComponent } from './directorio/directorio.component';
+import { config } from '@fullcalendar/core/internal';
+import { ConfiguracionComponent } from './configuracion/configuracion.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent, data: {title: 'Iniciar Sesión | LOF'}},
@@ -19,5 +21,6 @@ export const routes: Routes = [
     {path: 'herramientas', component: HomeHerramientasComponent, data: {title: 'Herramientas | LOF'}},
     {path: 'herramientas/usuarios', component: UsuariosComponent, data: {title: 'Herramientas - Usuarios | LOF'}},
     {path: 'herramientas/usuarios/nuevo', component: AddUserComponent, data: {title: 'Herramientas - Nuevo Usuario | LOF'}},
+    {path: 'configuracion', component: ConfiguracionComponent, data: {title: 'Configuración | LOF' }},
     {path:  '**', pathMatch:"full", redirectTo: 'login'}
 ];

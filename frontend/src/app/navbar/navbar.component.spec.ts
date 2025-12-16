7d41ef5f366e67f8d4420a37921df94886796da9
diego zamora <diego.zd03@gmail.com>
2025-12-15 23:05:09 -0600

    Update navbar.component.spec.ts

--- frontend/src/app/navbar/navbar.component.spec.ts
+++ frontend/src/app/navbar/navbar.component.spec.ts
@@ -1,16 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NavbarComponent } from './navbar.component';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
+  let authServiceSpy: jasmine.SpyObj<AuthService>;
+  let routerSpy: jasmine.SpyObj<Router>;
 
   beforeEach(async () => {
+    authServiceSpy = jasmine.createSpyObj('AuthService', [
+      'isAuthenticated',
+      'isAdmin',
+      'getCurrentUser',
+      'logout'
+    ]);
+
+    authServiceSpy.isAuthenticated.and.returnValue(true);
+    authServiceSpy.isAdmin.and.returnValue(false);
+    authServiceSpy.getCurrentUser.and.returnValue(null);
+
+    routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
     await TestBed.configureTestingModule({
-      imports: [NavbarComponent]
-    })
-    .compileComponents();
+      imports: [NavbarComponent], // standalone
+      providers: [
+        { provide: AuthService, useValue: authServiceSpy },
+        { provide: Router, useValue: routerSpy }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
